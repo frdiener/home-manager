746dc369c7fca2a671394866a79d9a3e1e4c22cf
vr1l <51481761+vr1l@users.noreply.github.com>
2024-04-20 12:59:37 +0200

    Changes to be committed: 	modified:   programs/neovim/config/copilot.lua

--- programs/neovim/config/copilot.lua
+++ programs/neovim/config/copilot.lua
@@ -1,3 +1,12 @@
+local function SuggestOneLine()
+  local suggestion = vim.fn['copilot#Accept']("")
+  local bar = vim.fn['copilot#TextQueuedForInsertion']()
+  -- This captures everything up to a newline
+  return bar:match("^[^\n]*")
+end
+local map = vim.keymap.set
+-- Map <Tab> to complete one line in insert mode
+map('i', '<Tab>', SuggestOneLine, {expr = true, remap = false})
 vim.api.nvim_set_keymap('i', '<C-l>', 'copilot#Accept("<CR>")', {expr = true, silent = true})
 vim.g.copilot_no_tab_map = true
 vim.g.copilot_assume_mapped = true
